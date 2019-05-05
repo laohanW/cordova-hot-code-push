1296182560d235aa8bb8e5900fc8ad6eb7a73670
zhouhan <zhouhan@lhcore.com>
2019-05-05 22:06:25 +0800

    add config.xml path

--- scripts/lib/chcpConfigXmlWriter.js
+++ scripts/lib/chcpConfigXmlWriter.js
@@ -82,7 +82,7 @@ function pathToIosConfigXml() {
  * @return {String} absolute path to config.xml file
  */
 function pathToAndroidConfigXml() {
-  return path.join(projectRoot, 'platforms', 'android', 'res', 'xml', 'config.xml');
+  return path.join(projectRoot, 'platforms', 'android','app','src','main', 'res', 'xml', 'config.xml');
 }
 
 /**
